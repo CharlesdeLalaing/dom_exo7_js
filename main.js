d74e7a305fdcc2b09019685a00e246e7b468820f
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-05-11 16:52:07 +0200

    push

--- main.js
+++ main.js
@@ -26,6 +26,6 @@ console.log(exo4);
 // ### 5. Affiche avec l'aide d'un foreach le contenu de chaque li avec la dernière lettre en majuscule 
 
 let exo5 = document.querySelectorAll('li');
-console.log(exo5.charAt(0).toUpperCase()+exo5.slice(1));
-
+// console.log(exo5.charAt(0).toUpperCase()+exo5.slice(1));
+console.log(exo5);
 // ### 6. Récupère le p dont la class est "grandParagraphe" de la div id "content", tu dois ici utiliser un getElementById et un getElementsByClassName
